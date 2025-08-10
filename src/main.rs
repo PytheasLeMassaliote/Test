d7f10c41f6cca29f93949f90e4c794158d5c7e25
Icham.Djilali-merzoug <ichemdjilali@hotmail.fr>
2025-08-10 10:37:42 +0200

    Ajout de code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let string = String::from("Test");
+    let reference = &string;
+    let mut vec_string = vec![String::from("Test3")];
+    vec_string.push(String::from("Salam les rhoya"));
+
+    println!("{} {}2 {:?}", string, reference, vec_string);
 }
